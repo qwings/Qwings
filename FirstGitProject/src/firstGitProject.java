9ac8e20f04ac6f9c6095b692177507745f9bf4bd
Qwings <qwings@yahoo.com>
2016-08-10 19:43:28 -0700

    darhas lowercase

--- FirstGitProject/src/firstGitProject.java
+++ FirstGitProject/src/firstGitProject.java
@@ -3,7 +3,7 @@ public class firstGitProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Darhas");
+		System.out.println("darhas");
 	}
 
 }
